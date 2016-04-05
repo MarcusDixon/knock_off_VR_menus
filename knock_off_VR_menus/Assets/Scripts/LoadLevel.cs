59a2efffff27a0f36c23ebbe0263c4cbf7ed081c
MarcusDixon <mdixon@csumb.edu>
2016-04-04 23:48:51 -0700

    initial commit of menus

---
+++ knock_off_VR_menus/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+
+    public void loadScene(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+/*
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            loadScene("level");
+        }
+    }*/ 
+}
---
+++ knock_off_VR_menus/Assets/Scripts/QuitGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour {
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Application.Quit();
+        }
+    }
+}
+
